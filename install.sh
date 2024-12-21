c96dd99314fb49a44bc379906094ff12fd79af65
yan-zhost <hf748256@gmail.com>
2024-12-21 10:23:39 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -92,7 +92,7 @@ install_theme() {
     read -r SELECT_THEME
     case "$SELECT_THEME" in
       1)
-        THEME_URL=$(echo -e "https://github.com/AlfaOffcial/thema/raw/main/C2.zip")
+        THEME_URL=$(echo -e "https://github.com/yan-zhost/install-panel/raw/main/C2.zip")
         break
         ;;
       2)
@@ -100,7 +100,7 @@ install_theme() {
         break
         ;;
       3)
-        THEME_URL=$(echo -e "https://github.com/AlfaOffcial/thema/raw/main/C3.zip")
+        THEME_URL=$(echo -e "https://github.com/yan-zhost/install-panel/raw/main/C3.zip")
         break
         ;; 
       x)
@@ -226,7 +226,7 @@ uninstall_theme() {
   echo -e "${BLUE}[+]                    DELETE THEME                 [+]${NC}"
   echo -e "${BLUE}[+] =============================================== [+]${NC}"
   echo -e "                                                       "
-  bash <(curl https://raw.githubusercontent.com/AlfaOffcial/thema/main/repair.sh)
+  bash <(curl https://raw.githubusercontent.com/yan-zhost/install-panel/main/repair.sh)
   echo -e "                                                       "
   echo -e "${GREEN}[+] =============================================== [+]${NC}"
   echo -e "${GREEN}[+]                 DELETE THEME SUKSES             [+]${NC}"
@@ -245,7 +245,7 @@ echo -e "${BLUE}[+] =============================================== [+]${NC}"
 echo -e "                                                                   "
 
 # Unduh file tema
-wget -O /root/C2.zip https://github.com/AlfaOffcial/thema/raw/main/C2.zip
+wget -O /root/C2.zip https://github.com/yan-zhost/install-panel/raw/main/C2.zip
 
 # Ekstrak file tema
 unzip /root/C2.zip -d /root/pterodactyl
